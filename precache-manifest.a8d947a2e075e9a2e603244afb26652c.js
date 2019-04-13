8906b18f467133d292eb42762753153292cfc97e
danlewis <flightofdan@gmail.com>
2019-04-13 11:10:08 +1000

    Updates

---
+++ precache-manifest.a8d947a2e075e9a2e603244afb26652c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c4861221d196ed0b6f9",
+    "url": "/flight/static/css/main.cfcc6aa0.chunk.css"
+  },
+  {
+    "revision": "6c4861221d196ed0b6f9",
+    "url": "/flight/static/js/main.6c92a20b.chunk.js"
+  },
+  {
+    "revision": "4beefbc4bade265a0b81",
+    "url": "/flight/static/js/runtime~main.0139e3eb.js"
+  },
+  {
+    "revision": "94656280f27c7fbc3a60",
+    "url": "/flight/static/js/2.a9791313.chunk.js"
+  },
+  {
+    "revision": "c92b85a5b907c70211f4ec25e29a8c4a",
+    "url": "/flight/static/media/favicon.c92b85a5.ico"
+  },
+  {
+    "revision": "57ecb70f13b0a0f78a4da7ebc8c37362",
+    "url": "/flight/static/media/aircraftatlas.57ecb70f.png"
+  },
+  {
+    "revision": "0b618b6082c21dba593a446946aa9675",
+    "url": "/flight/index.html"
+  }
+];
